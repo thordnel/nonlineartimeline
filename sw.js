1ab45b3a3e9e232c2cc0acd8a6ae8f283ed9019f
thordnel <thordnel@gmail.com>
2026-01-04 00:07:04 +0800

    bug fix on char cloud restore, update sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v7-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v8-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
