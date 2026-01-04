3661493a2d30540a1204e494548acde255d2af6b
thordnel <thordnel@gmail.com>
2026-01-04 16:01:58 +0800

    fix selection, one at a time, on chekhov's gun badge

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v13-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v14-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
