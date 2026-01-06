36344e17243219c7de7d881879f9b66f7f6d21fe
thordnel <thordnel@gmail.com>
2026-01-06 22:04:01 +0800

    modification counter, sticky buttons, cross-ref beats/cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v18-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v19-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
