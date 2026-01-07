6e7531d0e76cf2fcf0283d8c77bcfc25c05ae43d
thordnel <thordnel@gmail.com>
2026-01-07 18:27:23 +0800

    quick add and define rules

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v24-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v25-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
