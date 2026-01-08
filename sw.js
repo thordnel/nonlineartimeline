3796e540a6faa9493ec47469a96e44422febacf7
thordnel <thordnel@gmail.com>
2026-01-08 11:09:51 +0800

    minor changes and bug fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v31-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v32-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
