1a991bcf8e2512b0c0d935769d28c590b37172e8
thordnel <thordnel@gmail.com>
2026-01-08 11:29:18 +0800

    text and layout fix for ios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v32-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v33-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
