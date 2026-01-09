93f9c809da7c56e11906080c166c3ff0b89605c8
thordnel <thordnel@gmail.com>
2026-01-09 20:50:55 +0800

    sync optimization

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v42-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v43-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
