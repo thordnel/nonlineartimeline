eb1cf2ea67f5494f9b3f5825c8c59a9afdb40ebf
thordnel <thordnel@gmail.com>
2026-01-09 10:57:19 +0800

    show logs in console

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v39-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v40-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
