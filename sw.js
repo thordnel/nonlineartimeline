c0c6f0c7dccd2a5d7dd91bc7022966e4aa0a158e
thordnel <thordnel@gmail.com>
2026-01-10 12:57:13 +0800

    removed placeholders

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v50-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v51-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
