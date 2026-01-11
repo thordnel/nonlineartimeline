b59441089cc29c20a388dc48f8bc4920bb9efe86
thordnel <thordnel@gmail.com>
2026-01-11 19:41:03 +0800

    Prepend book title in notifs for multibook/multiprojects, same author.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v57-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v58-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
