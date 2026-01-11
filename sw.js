817f5a1d356f831e4967da6c43997a14362bb0e6
thordnel <thordnel@gmail.com>
2026-01-11 19:15:22 +0800

    Improved and multiline fields for details for longer texts in Narrative Items Trackers and New Ideas & World Logic Tracker spaces

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v55-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v56-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
