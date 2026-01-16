e7dec195d8c3b54865a6b64016c88085d1456db8
thordnel <thordnel@gmail.com>
2026-01-16 21:59:22 +0800

    beat duration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v60-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v61-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
