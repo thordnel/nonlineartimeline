99e443cddd363d18c488c65ea1f05b4ad9c7a7c6
thordnel <thordnel@gmail.com>
2026-01-17 17:47:05 +0800

    previous/next buttons for beat cycle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v63-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v64-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
