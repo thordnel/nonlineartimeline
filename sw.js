8433dc27f62a7d3a29f41cff42d8ee6202a36578
thordnel <thordnel@gmail.com>
2026-01-17 17:55:48 +0800

    swipe left and right gesture to move back or forward beat for mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v64-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v65-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
