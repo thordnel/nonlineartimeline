cd2785a88c212f3a76ac9aeda5da8b050f88345d
thordnel <thordnel@gmail.com>
2026-01-17 22:27:13 +0800

    idea or rule status management

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v65-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v66-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
