1a652cbedf9631b21666ac5baafe5d38a5a17348
thordnel <thordnel@gmail.com>
2026-01-17 23:22:35 +0800

    scroll to editor when edit button is clicked

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v67-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v68-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
