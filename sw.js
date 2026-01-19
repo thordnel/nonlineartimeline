92479f6625c84ee28bf24cf697c50c514b2ab6ae
thordnel <thordnel@gmail.com>
2026-01-19 23:18:59 +0800

    Origin Association File for deeplink

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v76-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v77-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
