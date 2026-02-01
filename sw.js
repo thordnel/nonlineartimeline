4390b4bf37bb4f943b5a1e1471a7cab93308bad4
thordnel <thordnel@gmail.com>
2026-02-01 14:37:53 +0800

    corrcets in manifest.json for deeplinking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v83-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v84-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
