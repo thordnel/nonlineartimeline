8c92c35ecd4ca8a5afd9844bad745e026804de9b
thordnel <thordnel@gmail.com>
2026-02-01 13:07:25 +0800

    added an hour to the event for ics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storylines-v81-offline'; // Increment version to force update
+const CACHE_NAME = 'storylines-v82-offline'; // Increment version to force update
 const ASSETS = [
   './',
   './index.html',
